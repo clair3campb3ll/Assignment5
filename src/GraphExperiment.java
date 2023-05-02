91df3327704d1e9280cecc51df2131ad419e058e
Claire Campbell <claire.18.campbell@gmail.com>
2023-05-02 09:59:57 +0200

    Added provided code and started the generation of the graphs

--- src/GraphExperiment.java
+++ src/GraphExperiment.java
@@ -1,5 +1,23 @@
+
+import java.util.Random;
+
 public class GraphExperiment {
+    private static final int[] vValues = {10,20,30,40,50};
+    private static final int[] eValues = {20,35,50,65,80,95};
+    private static final int cost = 10;
+
     public static void main(String[] args) {
+        int[] V = {10, 20, 30, 40, 50};
+        int[] E = {20, 35, 50, 65, 80, 95};
+        Random random = new Random();
+        Graph graph = new Graph();
+
+        for (int v : V){
+            for (int e: E){
+                
+            }
+        }
+
         
         
     }
